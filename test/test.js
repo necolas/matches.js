2b1c76968ee3229eca7185a82ffb6a6f6ced6a0f
Nicolas Gallagher <nicolasgallagher@twitter.com>
2013-05-07 12:41:21 -0700

    Fix code style in tests

--- test/test.js
+++ test/test.js
@@ -17,22 +17,27 @@ describe('matches(elem, selector)', function () {
     describe('when the element is `#one`', function () {
         it('returns true for selector `#one`', function () {
             expect(matches(one, '#one')).to.equal(true);
-        })
+        });
+
         it('returns true for selector `.foo`', function () {
             expect(matches(one, '.foo')).to.equal(true);
-        })
+        });
+
         it('returns true for selector `div`', function () {
             expect(matches(one, 'div')).to.equal(true);
-        })
+        });
+
         it('returns true for selector `div > div`', function () {
             expect(matches(one, 'div > div')).to.equal(true);
-        })
+        });
+
         it('returns false for selector `#two`', function () {
             expect(matches(one, '#two')).to.equal(false);
-        })
+        });
+
         it('returns false for selector `.bar`', function () {
             expect(matches(one, '.bar')).to.equal(false);
-        })
+        });
     });
 
     describe('when the element `#orphan` is not yet attached to the document', function () {
@@ -42,21 +47,26 @@ describe('matches(elem, selector)', function () {
 
         it('returns true for selector `#orphan`', function () {
             expect(matches(orphan, '#orphan'), 'testing').to.equal(true);
-        })
+        });
+
         it('returns true for selector `.foo`', function () {
             expect(matches(orphan, '.foo')).to.equal(true);
-        })
+        });
+
         it('returns true for selector `div`', function () {
             expect(matches(orphan, 'div')).to.equal(true);
-        })
+        });
+
         it('returns false for selector `#one`', function () {
             expect(matches(orphan, '#two')).to.equal(false);
-        })
+        });
+
         it('returns false for selector `.bar`', function () {
             expect(matches(orphan, '.bar')).to.equal(false);
-        })
+        });
+
         it('returns false for selector `body #orphan`', function () {
             expect(matches(orphan, 'body #orphan')).to.equal(false);
-        })
+        });
     });
 });
