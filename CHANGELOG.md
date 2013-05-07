6370766b7d709dbd97f367aa312f382cff99fd40
Nicolas Gallagher <nicolasgallagher@gmail.com>
2013-05-06 23:07:08 -0700

    v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,9 @@
 == HEAD
 
+== 1.0.1 (May 7, 2013)
+
 * Simplify AMD define
 
-== 1.0.0 (May 5th, 2013)
+== 1.0.0 (May 5, 2013)
 
 * Initial release
--- matches.js
+++ matches.js
@@ -1,4 +1,4 @@
-/*! matches.js v1.0.0 - Nicolas Gallagher - MIT license */
+/*! matches.js v1.0.1 - Nicolas Gallagher - MIT license */
 
 ;(function (global) {
 
